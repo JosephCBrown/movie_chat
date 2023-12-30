b3585a2437432e6752aafd47a44340eeea8aa6ec
Joseph Brown <87694098+JosephCBrown@users.noreply.github.com>
2023-12-29 21:05:26 -0800

    first commit

---
+++ api.py
@@ -0,0 +1,67 @@
+import requests
+import pandas
+import os
+from dotenv import load_dotenv
+
+
+#loads the .env that contains api keys
+load_dotenv()
+
+#assigns the api variable to the key in the .env  file
+API_KEY =(os.environ["API_KEY"])
+
+#url + the api key variable to access the data
+#url = f"https://api.themoviedb.org/3/movie/550?api_key={API_KEY}"
+
+#Movies Released in 2023
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&primary_release_year=2023&sort_by=popularity.desc"
+
+# Parameters for the API request
+params = {
+    "api_key": API_KEY,
+    "include_adult": "false",
+    "include_video": "false",
+    "language": "en-US",
+    "primary_release_year": "2023",
+    "sort_by": "popularity.desc"
+}
+
+#assigns the content type (output request)
+header={"Content-Type":"application/json",
+        "Accept-Encoding":"deflate",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Njc2NWEyN2ZkMDE1YzM4ZjZjZWNlZWFjODIwNTQwMyIsInN1YiI6IjY1OGY2N2M1ZDM2M2U1NTBmOTA5ZDcyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AZQ1bf7GxLyoMr4kIhINLSmJsjvHc3hhyYui4qbE7Cw"}
+
+response = requests.get(url, headers=header, params=params)
+data = response.json()
+print(data)  # Add this line to print the entire response
+
+# Initialize variables for pagination
+all_results = []
+page = 1
+max_pages = 500  # Maximum number of pages allowed by the API
+
+# Loop through each page of API results
+while page <= max_pages:
+    params["page"] = page
+    response = requests.get(url, headers=header, params=params)
+    data = response.json()
+
+    # Check if 'results' key is in the response
+    if 'results' in data:
+        all_results.extend(data['results'])
+    else:
+        print(f"No results or error found on page {page}, response: {data}")
+        break  # Exit the loop if 'results' is not found or there's an error
+
+    page += 1
+
+# Create a DataFrame from the results
+df = pandas.json_normalize(all_results)
+print(df)
+
+csv_file_path = '/Users/josephbrown/Desktop/Business/Data_Freelancer/DataUnity/jb_io/movie_chat_tool/file.csv'
+# Export the DataFrame to a CSV file
+df.to_csv(csv_file_path, index=False)
+
+print(f"Data exported to {csv_file_path}")
+
